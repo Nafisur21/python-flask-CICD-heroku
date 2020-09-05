9e492192fedb030a12bb660dfeecf607d47e3b50
nafisur <nafisur.rahman21@gmail.com>
2020-09-05 18:53:22 +0530

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # python-github-CI
 Example to check github action
+To run:-
+python -m myApp.app
+python myApp/app.py
+For pytest:-
+make myApp directory as source root
+in Terminal
+pytest
+or
+pytest -v
---
+++ myApp/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return "Hello, Nafisur Rahman!"
+
+
+@app.route("/test", methods=['POST'])
+def webhook():
+    data = request.get_json()
+    name = data['name']
+    title = data['title']
+    message = f'As-Salam-Alaikum {name} {title}'
+    return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
